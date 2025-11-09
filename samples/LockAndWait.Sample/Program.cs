57c81bb443d9a561f008f76320706b86c43408f7
Hamed Seifi <hamedseifi@outlook.com>
2025-11-09 15:05:02 +0330

    change sample code

--- samples/LockAndWait.Sample/Program.cs
+++ samples/LockAndWait.Sample/Program.cs
@@ -14,22 +14,21 @@ internal class Program
         await using var sp = services.BuildServiceProvider();
         var lockAndWaitService = sp.GetRequiredService<ILockAndWaitService>();
 
-        lockAndWaitService.AcquireAsync(Key, TimeSpan.FromSeconds(30)).GetAwaiter().GetResult();
+        await lockAndWaitService.AcquireAsync(Key, TimeSpan.FromSeconds(30));
 
 
         //In Another Command Or Application Can Wait For This Lock
         await DoAnything(sp);
     }
 
-    static Task DoAnything(IServiceProvider serviceProvider)
+    static async Task DoAnything(IServiceProvider serviceProvider)
     {
         using var scope = serviceProvider.CreateScope();
         var lockAndWaitService = scope.ServiceProvider.GetRequiredService<ILockAndWaitService>();
 
         Console.WriteLine("Waiting ...");
-        var lockIsFree = lockAndWaitService.WaitAsync(Key, timeout: TimeSpan.FromSeconds(5)).GetAwaiter().GetResult();
+        var lockIsFree = await lockAndWaitService.WaitAsync(Key, timeout: TimeSpan.FromSeconds(5));
 
         Console.WriteLine(lockIsFree ? "Lock is free , DoAnything Is Running" : "Timeout on Waiting for lock was timeout");
-        return Task.CompletedTask;
     }
 }
